c1971ce80b1eb4d270613f8298018bb69e86da5b
tribhu2001 <79187488+tribhu2001@users.noreply.github.com>
2026-05-21 21:14:06 +0530

    Updated Code

--- src/components/checkoutstepper.jsx
+++ src/components/checkoutstepper.jsx
@@ -1,7 +1,7 @@
-import React, { use, useRef } from 'react';
-import { useState}from 'react';
+import React, { useRef } from 'react';
+import { useState } from 'react';
 import { useEffect } from 'react';
-import '../index.css';
+import '../App.css';
 const Checkoutstepper = ({ stepsConfig = [] }) => {
 
     const [currentStep, setCurrentStep] = useState(1);
@@ -16,10 +16,10 @@ const Checkoutstepper = ({ stepsConfig = [] }) => {
 
     useEffect(() => {
         setMargins({
-            left: stepRef.current[0]?.offsetWidth/2,
-            right: stepRef.current[stepRef.current.length - 1]?.offsetWidth/2
+            left: stepRef.current[0]?.offsetWidth / 2,
+            right: stepRef.current[stepRef.current.length - 1]?.offsetWidth / 2
         })
-    },[stepRef,stepsConfig.length]);
+    }, [stepRef, stepsConfig.length]);
 
     const ActiveComponent = stepsConfig[currentStep - 1]?.Component;
 
@@ -41,21 +41,21 @@ const Checkoutstepper = ({ stepsConfig = [] }) => {
                 <div className='stepper'>
                     {stepsConfig.map((step, index) => (
                         <div key={step.name}
-                        ref={el => stepRef.current[index] = el}
-                        className={`step ${currentStep === index + 1 ? 'active' : ''} ${isComplete || index+1<currentStep ? 'completed' : ''}`}>
-                            <div className='step-number'>{index+1<currentStep ? '✓' : index + 1}</div>
+                            ref={el => stepRef.current[index] = el}
+                            className={`step ${currentStep === index + 1 ? 'active' : ''} ${isComplete || index + 1 < currentStep ? 'completed' : ''}`}>
+                            <div className='step-number'>{index + 1 < currentStep ? '✓' : index + 1}</div>
                             <div className="step-name">{step.name}</div>
                         </div>
                     ))}
                     <div className="progress-bar"
-                        style={{width: `calc(100% - ${margins.left + margins.right}px)`, marginLeft: `${margins.left}px`, marginRight: `${margins.right}px` }}
+                        style={{ width: `calc(100% - ${margins.left + margins.right}px)`, marginLeft: `${margins.left}px`, marginRight: `${margins.right}px` }}
                     >
-                        <div className="progress" style={{ width: `${(currentStep-1) / (stepsConfig.length - 1) * 100}%` }}>
-                            
+                        <div className="progress" style={{ width: `${(currentStep - 1) / (stepsConfig.length - 1) * 100}%` }}>
+
                         </div>
                     </div>
                 </div>
-                
+
                 <ActiveComponent />
                 {
                     !isComplete && <button className='next-button' onClick={() => handleNext()}>
